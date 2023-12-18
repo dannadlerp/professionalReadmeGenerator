f4097642d9b5b6117238dce177b3c48fda4cbfea
Dan Nadler <142226474+dannadlerp@users.noreply.github.com>
2023-12-18 13:50:22 -0500

    Add files via upload
    fixed the writefile function to properly generate a file. fixed bug that was preventing the generation - needed to stringify the answers array and inserted everything inside an init function as per instruction

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const { argv } = require('process');
+
+// TODO: Create a function to initialize app
+function init() {
+    
 //let answers = [];
 inquirer
 .prompt([
     {
         name: 'title',
         message: 'What is the title of the project?'
+    },
+    {
+        name: 'whatMotiv',
+        message: 'What was the motiv for the project?',
     },
     {
         name: 'why',
@@ -29,25 +39,41 @@ inquirer
     },
 ])
 
-.then(answers => {
-    answers.title = title;
-    answers.why = why;
-    answers.whatSolved = whatSolved;
-    answers.whatLearned = whatLearned;
-    answers.runInstructions = runInstructions;
-    answers.licence = licence;
-    console.log('Answers:', typeof(answers));
+.then(answers => { //assigns variables to each answer that is prompted
+    
+    title = `App Title: ${answers.title}`;
+    whatMotiv = `Motivation for the project: ${answers.whatMotiv}`;
+    why = `The reason for developement: ${answers.why}`;
+    whatSolved = `What did the App resolve? ${answers.whatSolved}`;
+    whatLearned = `What was learned from the App development: ${answers.whatLearned}`;
+    runInstructions = `Instructions to run App: ${answers.runInstructions}`;
+    licence = `App Licence: ${answers.licence}`;
+    
+    //below creates an array with all answers so it can be displayed in the new file
+    const questions = [title, whatMotiv, why, whatSolved, whatLearned, runInstructions, licence];
+    //below uses JSON to turn the array from [2] into a string
+    const jsonStringData = JSON.stringify(questions, null, 2);
+    const fileName = "newfile.txt";
+    console.log(fileName);
+    
+    // TODO: Create a function to write README file
+    fs.writeFileSync('./newfile.txt', jsonStringData), (err) =>
+    err ? console.error(err) : console.log('Success!') //logs any errors
+    
+    console.log('Answers:', answers);
 });
 
 // TODO: Create an array of questions for user input
 /* const questions = [title. whatMotiv, why, WhatSolved, WhatLearned, runInstructions, licence]; */
 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    /* fs.writeFile('README.md', process.argv)[2], (err) =>
+    err ? console.error(err) : console.log("hello") */
+    
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+}
 
 // Function call to initialize app
 init();
